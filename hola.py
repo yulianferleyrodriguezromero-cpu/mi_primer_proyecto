2da2ff9c1a8c079c9067171fb5a2223acb00b0e0
Yulian Rodriguez <yulianferleyrodriguezromero@gmail.com>
2025-11-11 21:03:57 -0500

    Primer commit: script de saludo en Python

---
+++ hola.py
@@ -0,0 +1,6 @@
+# hola.py
+# Autor: Yulian Ferley Rodríguez Romero
+# Descripción: Script básico que saluda al usuario
+
+nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
